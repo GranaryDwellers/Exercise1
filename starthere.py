a3f3f83216c3c66c2e4406327081ac23fdc5b034
Brian Jones <ba.jones@me.com>
2016-10-18 16:52:18 +0100

    forgot quotes to make a string

--- starthere.py
+++ starthere.py
@@ -4,6 +4,6 @@ This is a comment block - it does nothing, but makes things readable.
 the def statement defines a function.  A function is a block of code that is an instruction of how to do something
 '''
 def dothis():
-    print(hello world)
+    print("hello world")
     
 doThis() #this is a comment too - it calls the function (dothis) and does it. 
